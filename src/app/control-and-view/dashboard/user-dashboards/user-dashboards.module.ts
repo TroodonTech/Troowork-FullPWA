f7fef78270126123ffda252c013b485d92d59bab
rodneyThomas <rodney.johnthomas@troodontechnology.com>
2021-02-03 03:23:49 +0530

    issue fix

--- src/app/control-and-view/dashboard/user-dashboards/user-dashboards.module.ts
+++ src/app/control-and-view/dashboard/user-dashboards/user-dashboards.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MobileManagerDashBoardComponent } from './mobile-manager-dash-board/mobile-manager-dash-board.component';
-// import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
-// import { SupervisorDashboardComponent } from './supervisor-dashboard/supervisor-dashboard.component';
-// import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
-// import { SuperadminDashboardComponent } from './superadmin-dashboard/superadmin-dashboard.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [MobileManagerDashBoardComponent]
+  declarations: []
 })
 export class UserDashboardsModule { }
--- src/app/control-and-view/dashboard/user-welcome-pages/user-welcome-pages.module.ts
+++ src/app/control-and-view/dashboard/user-welcome-pages/user-welcome-pages.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MobileManagerWelcomeComponent } from './mobile-manager-welcome/mobile-manager-welcome.component';
-// import { SuperadminWelcomeComponent } from './superadmin-welcome/superadmin-welcome.component';
-// import { AdminWelcomeComponent } from './admin-welcome/admin-welcome.component';
-// import { SupervisorWelcomeComponent } from './supervisor-welcome/supervisor-welcome.component';
-// import { EmployeeWelcomeComponent } from './employee-welcome/employee-welcome.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ MobileManagerWelcomeComponent]
+  declarations: [ ]
 })
 export class UserWelcomePagesModule { }
